69f42d857e2e4e5a51b2cc38124176a667e3162a
aguinaldomuca <aguinaldomuca@gmail.com>
2026-05-26 15:45:23 +0100

    update

---
+++ frontend/android/app/src/main/java/com/devmuca/landingpage/MainActivity.java
@@ -0,0 +1,5 @@
+package com.devmuca.landingpage;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Age Muca',
+        short_name: 'AgeMuca',
+        theme_color: '#ffffff',
+      }
+    })
+  ],
   base: "/"
 })
